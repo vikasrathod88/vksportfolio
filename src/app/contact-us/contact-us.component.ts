2071753706d539f1a94ff35be1792b1821c30a95
vikasrathod88 <rvikas8830@gmail.com>
2025-04-03 13:14:54 +0530

    send mail

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,6 +1,7 @@
 import { NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -9,7 +10,87 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
-  onSubmit() {
-    alert("Message sent successfully! 🎉");
+  successMessage = '';
+  errorMessage = '';
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.invalid) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('access_key', '56f66bb4-c56c-49a6-a6b1-bb29c637ed28'); // Replace with your Web3Forms key
+    formData.append('name', contactForm.value.name);
+    formData.append('email', contactForm.value.email);
+    formData.append('message', contactForm.value.message);
+
+    this.http.post('https://api.web3forms.com/submit', formData)
+      .subscribe({
+        next: () => {
+          this.showMessage('success', 'Your message has been sent successfully!');
+          contactForm.resetForm();
+        },
+        error: () => {
+          this.showMessage('error', 'Something went wrong. Please try again!');
+        }
+      });
   }
+
+  showMessage(type: 'success' | 'error', message: string) {
+    if (type === 'success') {
+      this.successMessage = message;
+      this.errorMessage = '';
+    } else {
+      this.errorMessage = message;
+      this.successMessage = '';
+    }
+
+    // Hide message after 2 seconds
+    setTimeout(() => {
+      this.successMessage = '';
+      this.errorMessage = '';
+    }, 4000);
+  }
+
+
+
+
+  // successMessage = '';
+  // errorMessage = '';
+
+  // constructor(private http: HttpClient) {}
+
+  // onSubmit(contactForm: NgForm) {
+  //   debugger;
+  //   if (contactForm.invalid) {
+  //     return;
+  //   }
+
+  //   const formData = new FormData();
+  //   formData.append('access_key', '56f66bb4-c56c-49a6-a6b1-bb29c637ed28'); // Replace with your Web3Forms key
+  //   formData.append('name', contactForm.value.name);
+  //   formData.append('email', contactForm.value.email);
+  //   formData.append('message', contactForm.value.message);
+  //   debugger;
+  //   this.http.post('https://api.web3forms.com/submit', formData)
+  //     .subscribe({
+  //       next: () => {
+  //         this.successMessage = 'Your message has been sent successfully!';
+  //         this.errorMessage = '';
+  //         contactForm.resetForm();
+  //       },
+  //       error: () => {
+  //         this.successMessage = '';
+  //         this.errorMessage = 'Something went wrong. Please try again!';
+  //       }
+  //     });
+  // }
+
+
+
+
+
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
 import { ContactUsComponent } from "../contact-us/contact-us.component";
 import { NgxTypedJsModule } from 'ngx-typed-js';
+import { log } from 'node:console';
+
 
 @Component({
   selector: 'app-home',
@@ -9,6 +11,8 @@ import { NgxTypedJsModule } from 'ngx-typed-js';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+ngOnInit(): void {
+  console.log("ng On Init called...")
+}
 }
